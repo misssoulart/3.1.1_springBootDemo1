7a26e3ac4f27189b8a3ea0420990e5f552ca969d
Ekaterina Yakovenko <katya-sitnova@yandex.ru>
2022-09-18 15:51:22 +0300

    Initial commit

---
+++ src/main/java/com/example/springBootDemo1/SpringBootDemo1Application.java
@@ -0,0 +1,15 @@
+package com.example.springBootDemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemo1Application.class, args);
+	}
+
+}
+
+// подгрузилось, не меняла
